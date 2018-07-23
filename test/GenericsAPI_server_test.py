734348696f0c2bc5bac0f80f40515bc4363eeae9
Tianhao-Gu <tgu@anl.gov>
2018-07-23 13:01:49 -0500

    add initial tests

--- test/GenericsAPI_server_test.py
+++ test/GenericsAPI_server_test.py
@@ -5,6 +5,7 @@ import json  # noqa: F401
 import time
 import requests
 import inspect
+import shutil
 
 from os import environ
 try:
@@ -18,6 +19,8 @@ from biokbase.workspace.client import Workspace as workspaceService
 from GenericsAPI.GenericsAPIImpl import GenericsAPI
 from GenericsAPI.GenericsAPIServer import MethodContext
 from GenericsAPI.authclient import KBaseAuth as _KBaseAuth
+from DataFileUtil.DataFileUtilClient import DataFileUtil
+from GenomeFileUtil.GenomeFileUtilClient import GenomeFileUtil
 
 
 class GenericsAPITest(unittest.TestCase):
@@ -52,23 +55,65 @@ class GenericsAPITest(unittest.TestCase):
         cls.scratch = cls.cfg['scratch']
         cls.callback_url = os.environ['SDK_CALLBACK_URL']
 
+        cls.gfu = GenomeFileUtil(cls.callback_url)
+        cls.dfu = DataFileUtil(cls.callback_url)
+
+        suffix = int(time.time() * 1000)
+        cls.wsName = "test_kb_ke_apps_" + str(suffix)
+        cls.wsClient.create_workspace({'workspace': cls.wsName})
+        cls.prepare_data()
+
     @classmethod
     def tearDownClass(cls):
         if hasattr(cls, 'wsName'):
             cls.wsClient.delete_workspace({'workspace': cls.wsName})
             print('Test workspace was deleted')
 
+    @classmethod
+    def prepare_data(cls):
+        # upload genome object
+        genbank_file_name = 'minimal.gbff'
+        genbank_file_path = os.path.join(cls.scratch, genbank_file_name)
+        shutil.copy(os.path.join('data', genbank_file_name), genbank_file_path)
+
+        genome_object_name = 'test_Genome'
+        cls.genome_ref = cls.gfu.genbank_to_genome({'file': {'path': genbank_file_path},
+                                                    'workspace_name': cls.wsName,
+                                                    'genome_name': genome_object_name,
+                                                    'generate_ids_if_needed': 1
+                                                    })['genome_ref']
+
+        # upload expression matrix object
+        workspace_id = cls.dfu.ws_name_to_id(cls.wsName)
+        object_type = 'KBaseFeatureValues.ExpressionMatrix'
+        expression_matrix_object_name = 'test_expression_matrix'
+        expression_matrix_data = {'genome_ref': cls.genome_ref,
+                                  'scale': 'log2',
+                                  'type': 'level',
+                                  'data': {'row_ids': ['gene_1', 'gene_2', 'gene_3'],
+                                           'col_ids': ['condition_1', 'condition_2',
+                                                       'condition_3', 'condition_4'],
+                                           'values': [[0.1, 0.2, 0.3, 0.4],
+                                                      [0.3, 0.4, 0.5, 0.6],
+                                                      [None, None, None, None]]
+                                           },
+                                  'feature_mapping': {},
+                                  'condition_mapping': {}}
+        save_object_params = {
+            'id': workspace_id,
+            'objects': [{'type': object_type,
+                         'data': expression_matrix_data,
+                         'name': expression_matrix_object_name}]
+        }
+
+        dfu_oi = cls.dfu.save_objects(save_object_params)[0]
+        cls.expression_matrix_ref = str(dfu_oi[6]) + '/' + str(dfu_oi[0]) + '/' + str(dfu_oi[4])
+
     def getWsClient(self):
         return self.__class__.wsClient
 
     def getWsName(self):
-        if hasattr(self.__class__, 'wsName'):
-            return self.__class__.wsName
-        suffix = int(time.time() * 1000)
-        wsName = "test_GenericsAPI_" + str(suffix)
-        ret = self.getWsClient().create_workspace({'workspace': wsName})  # noqa
-        self.__class__.wsName = wsName
-        return wsName
+        return self.__class__.wsName
 
     def getImpl(self):
         return self.__class__.serviceImpl
